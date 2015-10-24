3aa80387cee35d02cb853b114f5b3a8e63376fc4
Venkata Bharathula <sivabharathula@ufl.edu>
2015-10-24 00:29:04 -0400

    new submission

--- untitledBloomberg/src/ExchangeClient.java
+++ untitledBloomberg/src/ExchangeClient.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 
 public class ExchangeClient {
@@ -36,6 +37,119 @@ public class ExchangeClient {
         this.bin = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
     }
+
+    public Orders getOrders(String ticker) throws IOException {
+        Orders myOrder = new Orders();
+        StringBuilder sb = new StringBuilder();
+        sb.append("ORDERS ").append(ticker);
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        if ((line = this.bin.readLine()) != null) {
+
+            String[] s = line.split(" ");
+            for (int i=1; i <s.length; i++) {
+                if (s[i].equals("BID")) {
+
+                }
+            }
+        }
+        return myOrder;
+    }
+
+
+    public void placeBid(String ticker, double price, int shares) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("BID ").append(ticker).append(" ").append(price).append(" ").append(shares);
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        double cash = 0.0;
+        if ((line = this.bin.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+
+    public void placeAsk(String ticker, double price, int shares) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("ASK ").append(ticker).append(" ").append(price).append(" ").append(shares);
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        if ((line = this.bin.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+
+    public void clearBid(String ticker) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("CLEAR_BID ").append(ticker);
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        if ((line = this.bin.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+
+    public void subscribe(TradingSystem mySystem) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("SUBSCRIBE");
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        double cash = 0.0;
+        while ((line = this.bin.readLine()) != null) {
+            mySystem.updateTrade(line);
+        }
+    }
+
+    public void unSubscribe() throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("UNSUBSCRIBE");
+        this.pout.println(sb.toString());
+        this.pout.flush();
+    }
+
+
+    public HashMap<String,Double> getDividends() throws IOException {
+        HashMap<String,Double> result = new HashMap<String,Double>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("MY_SECURITIES");
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        if ((line = this.bin.readLine()) != null) {
+            String[] s = line.split(" ");
+            int i=0;
+            while(i < s.length -2) {
+                result.put(s[i], Double.parseDouble(s[i+2]));
+            }
+        }
+        return result;
+    }
+
+
+    public void clearAsk(String ticker) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("CLEAR_ASK ").append(ticker);
+        this.pout.println(sb.toString());
+        this.pout.flush();
+        String line;
+        if ((line = this.bin.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
 
 
     public double getCash() throws IOException {
---
+++ untitledBloomberg/src/Orders.java
@@ -0,0 +1,6 @@
+/**
+ * Created by venkata on 10/24/15.
+ */
+public class Orders {
+
+}
